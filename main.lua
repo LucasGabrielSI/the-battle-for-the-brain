9227a995ef8f69606cd186efe7949c4bb266b113
LucasGabriel <lucasiguape16@gmail.com>
2018-09-10 19:36:56 -0300

    add main lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( 'composer' )
+
+-- Gerador de numero aleatorios
+math.randomseed( os.time() )
+
+-- Remover a barra de status do celular
+display.setStatusBar( display.HiddenStatusBar )
+
+composer.gotoScene( "scene1" )
